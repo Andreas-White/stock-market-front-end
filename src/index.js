ba256b92b5307953704ef33a9511a6b0e7c3d7d4
Andreas Manos <antmanos42@gmail.com>
2022-01-25 17:12:32 +0100

    fixed issue with stacking fetch requests by rerendering windo on each search

--- src/index.js
+++ src/index.js
@@ -5,9 +5,34 @@ const inputStartDate = document.querySelector('.startDate');
 const inputEndDate = document.querySelector('.endDate');
 const inputSearch = document.querySelector('.searchStock');
 
-let name = 'AAPL';
-let initStartDate = getStartDate();
-let initEndDate = getEndDate();
+let sessionStorage = window.sessionStorage;
+
+// Get saved data from sessionStorage
+let stockName = sessionStorage.getItem('stockName');
+let stockStartDate = sessionStorage.getItem('stockStartDate');
+let stockEndDate = sessionStorage.getItem('stockEndDate');
+
+let name = stockName;
+let initStartDate = stockStartDate;
+let initEndDate = stockEndDate;
+
+if (name !== '' && name !== null) {
+  sessionStorage.removeItem('stockName');
+} else {
+  name = 'AAPL';
+}
+
+if (initStartDate !== '' && initStartDate !== null) {
+  sessionStorage.removeItem('stockStartDate');
+} else {
+  initStartDate = getStartDate();
+}
+
+if (initEndDate !== '' && initEndDate !== null) {
+  sessionStorage.removeItem('stockEndDate');
+} else {
+  initEndDate = getEndDate();
+}
 
 await chart.createChart(name, initStartDate, initEndDate);
 
@@ -16,15 +41,11 @@ inputSearch.addEventListener('click', async () => {
   let startDate = inputStartDate.value;
   let endDate = inputEndDate.value;
 
-  if (startDate === '') {
-    startDate = getStartDate();
-  }
+  sessionStorage.setItem('stockName', name);
+  sessionStorage.setItem('stockStartDate', startDate);
+  sessionStorage.setItem('stockEndDate', endDate);
 
-  if (endDate === '') {
-    endDate = getEndDate();
-  }
-
-  await chart.createChart(name, startDate, endDate);
+  location.reload();
 });
 
 function getStartDate() {
